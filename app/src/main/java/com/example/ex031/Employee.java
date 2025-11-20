3a68c1c432e8c9b4e0bc05015de4f2178389c05b
ng5134 <ng5134@bs.amalnet.k12.il>
2025-11-20 19:45:05 +0200

    without setters0 they didnt ask for it

--- app/src/main/java/com/example/ex031/Employee.java
+++ app/src/main/java/com/example/ex031/Employee.java
@@ -19,15 +19,12 @@ public abstract class Employee {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public double getBaseSalary() {
         return baseSalary;
     }
 
-    public void setBaseSalary(double baseSalary) {
-        this.baseSalary = baseSalary;
+    public abstract double calculateMonthlySalary{
+
     }
+
 }
