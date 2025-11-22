90f4321e2978fca3c1d2135042e8259cf48a3461
ng5134 <ng5134@bs.amalnet.k12.il>
2025-11-22 14:24:07 +0200

    manager-extends from Employee and implements from BounsEligible

---
+++ app/src/main/java/com/example/ex031/Manager.java
@@ -0,0 +1,15 @@
+package com.example.ex031;
+
+public class Manager extends Employee implements BonusEligible{
+    private String department;
+    private double managementBonusPercentage;
+
+    public Manager(int id, String name,double baseSalary,String department, double managementBonusPercentage){
+        super(id,name,baseSalary);
+        this.department=department;
+        this.managementBonusPercentage=managementBonusPercentage;
+
+    }
+    @Override
+    double calculateMonthlySalary:
+}
