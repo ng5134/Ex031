388d29353f3d8f1cd70fba2e64bb0a6096f10edf
ng5134 <ng5134@bs.amalnet.k12.il>
2025-11-22 15:11:44 +0200

    list of employee with everything

--- app/src/main/java/com/example/ex031/MainActivity.java
+++ app/src/main/java/com/example/ex031/MainActivity.java
@@ -20,10 +20,10 @@ public class MainActivity extends AppCompatActivity {
 
         List<Employee> allEmployees =new ArrayList<>();
 
-        allEmployees.add(new Manager(1,));
-        allEmployees.add(new Salesperson());
-        allEmployees.add(new HourlyEmployee());
-        allEmployees.add(new HourlyEmployee());
-        allEmployees.add(new Manager(2,));
+        allEmployees.add(new Manager(1,"Albert",40000,"Cyber-AndroidS",0.5));
+        allEmployees.add(new Manager(2,"Itzchak",39000,"computer science",0.6));
+        allEmployees.add(new Salesperson(3,"noa",35000,0.4,80000));
+        allEmployees.add(new HourlyEmployee(4,"daniel",500,250,120));
+        allEmployees.add(new HourlyEmployee(5,"Abraham",90,160,35));
     }
 }
