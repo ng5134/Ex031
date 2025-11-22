1cd5d4ba0357a5e6cb1145ded89076b1a1a8afe1
ng5134 <ng5134@bs.amalnet.k12.il>
2025-11-22 14:11:58 +0200

    Hourly Employee- extedns from employee

---
+++ app/src/main/java/com/example/ex031/HourlyEmployee.java
@@ -0,0 +1,18 @@
+package com.example.ex031;
+
+public class HourlyEmployee extends Employee {
+    private double hoursWorked;
+    private double hourlyRate;
+
+    public HourlyEmployee(int id,String name,double baseSalary,double hoursWorked,double hourlyRate){
+        super (id,name,baseSalary);
+        this.hoursWorked=hoursWorked;
+        this.hourlyRate=hourlyRate;
+
+    }
+    @Override
+    public dounle calculateMonthlySalary(){
+        return hoursWorked *hourlyRate
+    }
+
+}
